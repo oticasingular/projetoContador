61415e913ab3aabbe3a006ec30745ce8a6141f36
Ana Silveira <almeida.oticasingular@gmail.com>
2025-03-30 17:41:55 -0300

    inclusão de exercicio de contador.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      titulo: "Contador de clicks",
+      contador: 0, 
+    }
+    this.aumentar = this.aumentar.bind(this);
+    this.diminuir = this.diminuir.bind(this);
+  }
+
+  aumentar(){
+    this.setState((contadorAtual) => ({
+      contador : contadorAtual.contador +1,
+    }));
+
+    console.log("aumentou 1")
+  }
+
+  diminuir() {
+    this.setState((contadorAtual) => ({
+      contador: contadorAtual.contador -1,
+    }));
+    console.log("diminuiu 1")
+  }
+
+  render(){
+    return (
+      <div className="estilo">
+        <h1 > {this.state.titulo} </h1>
+        <h3> {this.state.contador} </h3>
+        <div className="botoes">
+          <button onClick={this.aumentar} > aumentar contador + </button>
+          <button onClick={this.diminuir} > diminuir contador - </button>
+        </div>
+      </div>
+    )
+  }
+
 }
 
 export default App;
